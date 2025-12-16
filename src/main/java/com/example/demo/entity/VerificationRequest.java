153e0eace708851803ac8465a796108b8e945382
bala-amypo <bala@amypo.in>
2025-12-16 15:29:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationRequest.java
+++ src/main/java/com/example/demo/entity/VerificationRequest.java
@@ -6,10 +6,10 @@ public class VerificationRequest{
     private Long id;
     private Long credentialId;
     private String requestBy;
-    private String ver;
-    private String organization;
-    private Boolean active;
-    private LocalDateTime createdAt;
+    private String verificationMethod;
+    private String status;
+    private LocalDateTime verifiedAt;
+    private String resultMessage;
 
     public Long getId(){
         return id;
@@ -18,31 +18,31 @@ public class VerificationRequest{
         this.id=id;
     }
 
-    public String getHolderId(){
-        return holderId;
+    public String getCredentialId(){
+        return credentialId;
     }
-    public void setHolderId(String holderId){
-        this.holderId=holderId;
-    }
-
-    public String getFullName(){
-        return fullName;
-    }
-    public void setFullName(String fullName){
-        this.fullName=fullName;
+    public void setCredentialId(String credentialId){
+        this.credentialId=credentialId;
     }
 
-    public String getEmail(){
-        return email;
+    public String getRequestBy(){
+        return requestBy;
     }
-    public void setEmail(String email){
-        this.email=email;
+    public void setRequestBy(String requestBy){
+        this.requestBy=requestBy;
     }
 
-    public String getOrganization(){
-        return organization;
+    public String getVerificationMethod(){
+        return verificationMethod;
     }
-    public void setOrganization(String organization){
+    public void setVerificationMethod(String verificationMethod){
+        this.verificationMethod=verificationMethod;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+    public void setStatus(String organization){
         this.organization=organization;
     }
 
