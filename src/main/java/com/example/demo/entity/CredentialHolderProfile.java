7a1f76e9fbb75c3b7f18057c15a43af0cc857528
bala-amypo <bala@amypo.in>
2025-12-16 14:41:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/CredentialHolderProfile.java
+++ src/main/java/com/example/demo/entity/CredentialHolderProfile.java
@@ -6,5 +6,60 @@ public class CredentialHolderProfile{
     private String holderId;
     private String fullName;
     private String email;
-    private String organization
+    private String organization;
+    private Boolean active;
+    private LocalDateTime creatAt;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id=id;
+    }
+
+    public String getId(){
+        return holderId;
+    }
+
+    public void setId(){
+        this.holderId=holderId;
+    }
+    public String getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id=id;
+    }
+    public String getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id=id;
+    }
+    public String getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id=id;
+    }
+
+    public Boolean getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id=id;
+    }
+
+    public LocalDateTime getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id=id;
+    }
 }
