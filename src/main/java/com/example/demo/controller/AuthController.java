c5654cf91f37568ae9fdf807b1e764e3103f4d06
bala-amypo <bala@amypo.in>
2025-12-16 16:09:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,23 @@
-package com.example.demo.controller;
+package com.example.collectiondb.controller;
 
-public class AuthController{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.collectiondb.entity.studentEntity;
+import com.example.collectiondb.service.studentService;
+
+
+@RestController
+public class studentController {
+    
+    @Autowired
+    studentService stuser;
+
+    @PostMapping("/path")
+    public studentEntity studatas(@RequestBody studentEntity student) {
+        return stuser.saveData(student);
+    }
     
 }
