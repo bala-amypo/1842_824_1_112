1e754a2c8367687a464fae6d0a55c0718eb53afc
bala-amypo <bala@amypo.in>
2025-12-16 15:06:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/CredentialRecord.java
+++ src/main/java/com/example/demo/entity/CredentialRecord.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 public class CredentialRecord{
     
     private Long id;
-    private String holderId;
+    private Long holderId;
     private String credentialCode;
     private String title;
     private String issuer;
@@ -13,3 +13,53 @@ public class CredentialRecord{
     private String credentialType;
     private String status;
     private String  metadataJson;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public Long getHolderId(){
+        return holderId;
+    }
+    public void setHolderId(String holderId){
+        this.holderId=holderId;
+    }
+
+    public String getCredentialCode(){
+        return fullName;
+    }
+    public void setCredentialCode(String fullName){
+        this.fullName=fullName;
+    }
+
+    public String getTitle(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getOrganization(){
+        return organization;
+    }
+    public void setOrganization(String organization){
+        this.organization=organization;
+    }
+
+    public Boolean getActive(){
+        return active;
+    }
+    public void setActive(Booleanctive){
+        this.active=active;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime creatAt){
+        this.createdAt=createdAt;
+    }
+} 
