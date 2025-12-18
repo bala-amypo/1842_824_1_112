a4b2d13ef4ecbfcefcb0510717c70febcb85c494
bala-amypo <bala@amypo.in>
2025-12-18 13:01:04 +0000

    User commit

--- src/main/java/com/example/demo/service/impls/AuditTrailRecordImpls.java
+++ src/main/java/com/example/demo/service/impls/AuditTrailRecordImpls.java
@@ -17,8 +17,8 @@ public class AuditTrailRecordImpls {
         return atrr.save(record);
     }
 
-    public Long getLogsByCredential(Long credentialId){
-        return atrr.findById(credentialId);
+    public List<AuditTrailRecord> getLogsByCredential(Long credentialId){
+        return atrr.findByCredentialId(credentialId);
     }
 
     public List<AuditTrailRecord> getAllLogs(){
