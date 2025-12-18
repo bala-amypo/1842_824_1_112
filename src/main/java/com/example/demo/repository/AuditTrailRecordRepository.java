d2a07125778217edd4819786f35f14b82f09b2ff
bala-amypo <bala@amypo.in>
2025-12-18 12:55:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/AuditTrailRecordRepository.java
+++ src/main/java/com/example/demo/repository/AuditTrailRecordRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.AuditTrailRecord;
+import java.util.List;
+import java.lang.Long;
 
 @Repository
 public interface AuditTrailRecordRepository extends JpaRepository<AuditTrailRecord, Long>{
