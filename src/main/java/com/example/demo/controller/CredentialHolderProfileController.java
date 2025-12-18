5d3fcf7080e456752d7c05cf73420620470b7670
bala-amypo <bala@amypo.in>
2025-12-18 14:54:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CredentialHolderProfileController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.CredentialHolderProfile;
+import com.example.demo.service.CredentialHolderProfileService;
+import java.util.List;
+
+@RestController
+public class CredentialHolderProfileController {
+    @Autowired
+    private CredentialHolderProfileService chps;
+
+    @PostMapping("/audit")
+    public CredentialHolderProfile addCredentialHolderProfile(@RequestBody CredentialHolderProfile chp){
+        return chps.logEvent(chp);
+    }
+
+    @GetMapping("/{credentialId}")
+    public List<AuditTrailRecord> getByCredential(@PathVariable Long credentialId){
+        return atrs.getLogsByCredential(credentialId);
+    }
+
+    @GetMapping
+    public List<AuditTrailRecord> getAll(){
+        return atrs.getAllLogs();
+    }
+}
