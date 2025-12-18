781bc34bb60d5eaa339229cbb94ab7c147852362
bala-amypo <bala@amypo.in>
2025-12-18 00:59:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.example.collectiondb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.AuditTrailRecord;
+import com.example.demo.service.AuditTrailRecordService;
+
+
+@RestController
+public class AuditTrailRecordController {
+
+}
