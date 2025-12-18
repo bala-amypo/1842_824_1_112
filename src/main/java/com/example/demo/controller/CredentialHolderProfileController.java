fd326249f95f52cfa9b5052ffbf4394b085bbf64
bala-amypo <bala@amypo.in>
2025-12-18 16:06:05 +0000

    User commit

--- src/main/java/com/example/demo/controller/CredentialHolderProfileController.java
+++ src/main/java/com/example/demo/controller/CredentialHolderProfileController.java
@@ -1,41 +1,46 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.CredentialHolderProfile;
 import com.example.demo.service.CredentialHolderProfileService;
-import java.lang.Long;
+
 import java.util.List;
 
 @RestController
 public class CredentialHolderProfileController {
+
     @Autowired
     private CredentialHolderProfileService chps;
 
     @PostMapping("/holders")
-    public CredentialHolderProfile createHolder(@RequestBody CredentialHolderProfile chp){
+    public CredentialHolderProfile createHolder(
+            @RequestBody CredentialHolderProfile chp) {
         return chps.createHolder(chp);
     }
 
-    @GetMapping("/{id}")
-    public CredentialHolderProfile getHolderById(@PathVariable Long id){
+    @GetMapping("/holders/{id}")
+    public CredentialHolderProfile getHolderById(
+            @PathVariable Long id) {
         return chps.getHolderById(id);
     }
 
-    @GetMapping
-    public CredentialHolderProfile getAllHolders(){
+    @GetMapping("/holders")
+    public List<CredentialHolderProfile> getAllHolders() {
         return chps.getAllHolders();
     }
 
-    @PutMapping("/{id}/status")
-    public CredentialHolderProfile updateHolderStatus(@PathVariable Long id, @RequestParam boolean active){
+    @PutMapping("/holders/{id}/status")
+    public CredentialHolderProfile updateHolderStatus(
+            @PathVariable Long id,
+            @RequestParam boolean active) {
         return chps.updateHolderStatus(id, active);
     }
 }
