a3222a5e47fc048b30b3d4fba9913b265d330b67
bala-amypo <bala@amypo.in>
2025-12-18 14:21:06 +0000

    User commit

--- src/main/java/com/example/demo/service/CredentialHolderProfileService.java
+++ src/main/java/com/example/demo/service/CredentialHolderProfileService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.AuditTrailRecord;
 
-public interface AuditTrailRecordService {
-    createHolder
+public interface CredentialHolderProfile {
+    CredentialHolderProfile createHolder(CredentialHolderProfile profile);
+    getHolderById(Long id);
 }
