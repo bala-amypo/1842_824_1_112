8056324a056e3af3cfc0afead2c1ac5af5fc0a8c
bala-amypo <bala@amypo.in>
2025-12-18 01:09:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impls/AuditTrailRecordImpls.java
+++ src/main/java/com/example/demo/service/impls/AuditTrailRecordImpls.java
@@ -2,3 +2,19 @@ package com.example.demo.service.impls;
 
 import com.example.demo.service.AuditTrailRecordService;
 import com.example.demo.repository.AuditTrailRecordRepository;
+import org.springframework.stereotype.service;
+
+@Service
+
+public class AuditTrailRecordImpls implements AuditTrailRecordService{
+    private final AuditTrailRecordRepository auditRepo;
+
+    public AuditTrailRecordImpls(AuditTrailRecordRepository auditRepo){
+        this.auditRepo=auditRepo;
+    }
+
+    @Override
+    public void logEvent(AuditTrailRecord record){
+        auditRepo
+    }
+}
