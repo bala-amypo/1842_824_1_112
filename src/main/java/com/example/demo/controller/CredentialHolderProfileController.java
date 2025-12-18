51a22a1c2933d5f05bcf08d774dff3821c52bd2c
bala-amypo <bala@amypo.in>
2025-12-18 15:15:05 +0000

    User commit

--- src/main/java/com/example/demo/controller/CredentialHolderProfileController.java
+++ src/main/java/com/example/demo/controller/CredentialHolderProfileController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.CredentialHolderProfile;
 import com.example.demo.service.CredentialHolderProfileService;
+import java.lang.Long;
 import java.util.List;
 
 @RestController
