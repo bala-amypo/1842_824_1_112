fb47d6b240629d0f8bacc304ebc96ceba0a75f2e
bala-amypo <bala@amypo.in>
2025-12-19 01:16:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impls/CredentialRecordImpls.java
@@ -0,0 +1,52 @@
+package com.example.demo.service.impls;
+
+import com.example.demo.service.Service;
+import com.example.demo.repository.CredentialRecordRepository;
+import com.example.demo.entity.CredentialRecord;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class CredentialRecordImpls implements CredentialRecordService {
+
+    @Autowired
+    private Repository chpr;
+
+    @Override
+    public CredentialHolderProfile createHolder(
+            CredentialHolderProfile profile) {
+        return chpr.save(profile);
+    }
+
+    @Override
+    public CredentialHolderProfile getHolderById(Long id) {
+        return chpr.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Holder not found"));
+    }
+
+    @Override
+    public List<CredentialHolderProfile> getAllHolders() {
+        return chpr.findAll();
+    }
+
+    @Override
+    public CredentialHolderProfile findByHolderId(String holderId) {
+        return chpr.findByHolderId(holderId);
+    }
+
+    @Override
+    public CredentialHolderProfile updateHolderStatus(
+            Long id, boolean active) {
+
+        CredentialHolderProfile holder = chpr.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Holder not found"));
+
+        holder.setActive(active);
+        return chpr.save(holder);
+    }
+}
