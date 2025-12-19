34428bc9b039e2cf6365d86b777695d31e851147
bala-amypo <bala@amypo.in>
2025-12-19 05:46:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/VerificationRuleController.java
+++ src/main/java/com/example/demo/controller/VerificationRuleController.java
@@ -15,21 +15,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/rules")
 public class VerificationRuleController {
+
     @Autowired
     private VerificationRuleService vrs;
 
     @PostMapping
-    public VerificationRule addAuditTrailRecord(@RequestBody VerificationRule vr){
-        return vrs.logEvent(vr);
+    public VerificationRule createRule(@RequestBody VerificationRule rule) {
+        return vrs.createRule(rule);
     }
 
-    @GetMapping("/{credentialId}")
-    public List<AuditTrailRecord> getByCredential(@PathVariable Long credentialId){
-        return atrs.getLogsByCredential(credentialId);
+    @PutMapping("/{id}")
+    public VerificationRule updateRule( @PathVariable Long id,@RequestBody VerificationRule rule) {
+        return vrs.updateRule(id, rule);
+    }
+
+    @GetMapping("/active")
+    public List<VerificationRule> getActiveRules() {
+        return vrs.getActiveRules();
     }
 
     @GetMapping
-    public List<AuditTrailRecord> getAll(){
-        return atrs.getAllLogs();
+    public List<VerificationRule> getAllRules() {
+        return vrs.getAllRules();
     }
 }
--- src/main/java/com/example/demo/repository/VerificationRuleRepository.java
+++ src/main/java/com/example/demo/repository/VerificationRuleRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.VerificationRule;
+import java.util.List;
 
 @Repository
 public interface VerificationRuleRepository extends JpaRepository<VerificationRule, Long>{
