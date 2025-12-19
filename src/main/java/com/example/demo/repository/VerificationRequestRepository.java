343c5e4c3dc59a0a787bc1c6440da263ad6ede28
bala-amypo <bala@amypo.in>
2025-12-19 10:38:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/VerificationRequestRepository.java
+++ src/main/java/com/example/demo/repository/VerificationRequestRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.VerificationRequest;
+import java.util.List;
 
 @Repository
 public interface VerificationRequestRepository extends JpaRepository<VerificationRequest, Long>{
-
+    List<VerificationRequest> getRequestsByCredential(Long credentialId);
 }
