7bd04cbc184ac52e4a47d22146246d7067763270
bala-amypo <bala@amypo.in>
2025-12-19 01:10:29 +0000

    User commit

--- src/main/java/com/example/demo/service/CredentialRecordService.java
+++ src/main/java/com/example/demo/service/CredentialRecordService.java
@@ -4,5 +4,9 @@ import java.util.List;
 import com.example.demo.entity.CredentialRecord;
 
 public interface CredentialRecordService{
-
+    createCredential(CredentialRecord record);
+    updateCredential(Long id, CredentialRecord updated);
+    getCredentialsByHolder(Long holderId);
+    getCredentialByCode(String code);
+    getAllCredentials();
 }
