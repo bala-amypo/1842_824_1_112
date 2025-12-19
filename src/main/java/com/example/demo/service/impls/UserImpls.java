4fc5976dcb3c0ae5bf9aa2ce67c9ce142aee0bc6
bala-amypo <bala@amypo.in>
2025-12-19 07:57:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impls/UserImpls.java
+++ src/main/java/com/example/demo/service/impls/UserImpls.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.lang.Long;
 
 @Service
 public class UserImpls implements UserService {
