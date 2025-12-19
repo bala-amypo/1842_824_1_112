0368f316181736c38e6af7536e906f444cc17763
bala-amypo <bala@amypo.in>
2025-12-19 09:23:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impls/VerificationRequestImpls.java
@@ -0,0 +1,51 @@
+package com.example.demo.service.impls;
+
+import com.example.demo.service.CredentialRecordService;
+import com.example.demo.repository.CredentialRecordRepository;
+import com.example.demo.entity.CredentialRecord;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class CredentialRecordImpls implements CredentialRecordService {
+
+    @Autowired
+    private CredentialRecordRepository crr;
+
+    @Override
+    public CredentialRecord createCredential(CredentialRecord record) {
+        if (record.getStatus() == null) {
+            record.setStatus("VALID");
+        }
+        return crr.save(record);
+    }
+
+    @Override
+    public CredentialRecord updateCredential(Long id, CredentialRecord updated) {
+        CredentialRecord existing = crr.findById(id).orElseThrow(() ->new RuntimeException("Credential not found"));
+
+        existing.setCredentialCode(updated.getCredentialCode());
+        existing.setHolderId(updated.getHolderId());
+        existing.setStatus(updated.getStatus());
+
+        return crr.save(existing);
+    }
+
+    @Override
+    public List<CredentialRecord> getCredentialsByHolder(Long holderId) {
+        return crr.findByHolderId(holderId);
+    }
+
+    @Override
+    public CredentialRecord getCredentialByCode(String code) {
+        return crr.findByCredentialCode(code);
+    }
+
+    @Override
+    public List<CredentialRecord> getAllCredentials() {
+        return crr.findAll();
+    }
+}
