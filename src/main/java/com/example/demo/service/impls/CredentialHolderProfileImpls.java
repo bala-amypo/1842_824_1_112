4a5270ccf17796dbf63e7cba09852a8cfb0e7286
bala-amypo <bala@amypo.in>
2025-12-23 05:15:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impls/CredentialHolderProfileImpls.java
+++ src/main/java/com/example/demo/service/impls/CredentialHolderProfileImpls.java
@@ -1,47 +1,47 @@
-package com.example.demo.service.impls;
+    package com.example.demo.service.impls;
 
-import com.example.demo.service.CredentialHolderProfileService;
-import com.example.demo.repository.CredentialHolderProfileRepository;
-import com.example.demo.entity.CredentialHolderProfile;
+    import com.example.demo.service.CredentialHolderProfileService;
+    import com.example.demo.repository.CredentialHolderProfileRepository;
+    import com.example.demo.entity.CredentialHolderProfile;
 
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
+    import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
+    import java.util.List;
 
-@Service
-public class CredentialHolderProfileImpls implements CredentialHolderProfileService {
+    @Service
+    public class CredentialHolderProfileImpls implements CredentialHolderProfileService {
 
-    @Autowired
-    private CredentialHolderProfileRepository chpr;
+        @Autowired
+        private CredentialHolderProfileRepository chpr;
 
-    @Override
-    public CredentialHolderProfile createHolder(CredentialHolderProfile profile) {
-        return chpr.save(profile);
+        @Override
+        public CredentialHolderProfile createHolder(CredentialHolderProfile profile) {
+            return chpr.save(profile);
+        }
+
+        @Override
+        public CredentialHolderProfile getHolderById(Long id) {
+            return chpr.findById(id).orElseThrow(() ->new RuntimeException("Holder not found"));
+        }
+
+        @Override
+        public List<CredentialHolderProfile> getAllHolders() {
+            return chpr.findAll();
+        }
+
+        @Override
+        public CredentialHolderProfile findByHolderId(String holderId) {
+            return chpr.findByHolderId(holderId);
+        }
+
+        @Override
+        public CredentialHolderProfile updateHolderStatus(
+                Long id, boolean active) {
+
+            CredentialHolderProfile holder = chpr.findById(id).orElseThrow(() -> new RuntimeException("Holder not found"));
+
+            holder.setActive(active);
+            return chpr.save(holder);
+        }
     }
-
-    @Override
-    public CredentialHolderProfile getHolderById(Long id) {
-        return chpr.findById(id).orElseThrow(() ->new RuntimeException("Holder not found"));
-    }
-
-    @Override
-    public List<CredentialHolderProfile> getAllHolders() {
-        return chpr.findAll();
-    }
-
-    @Override
-    public CredentialHolderProfile findByHolderId(String holderId) {
-        return chpr.findByHolderId(holderId);
-    }
-
-    @Override
-    public CredentialHolderProfile updateHolderStatus(
-            Long id, boolean active) {
-
-        CredentialHolderProfile holder = chpr.findById(id).orElseThrow(() -> new RuntimeException("Holder not found"));
-
-        holder.setActive(active);
-        return chpr.save(holder);
-    }
-}
