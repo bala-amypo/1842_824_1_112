852cbaa1920f5b19e3303ec590c451225cbcc789
bala-amypo <bala@amypo.in>
2025-12-25 10:48:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impls/VerificationRequestImpls.java
+++ src/main/java/com/example/demo/service/impls/VerificationRequestImpls.java
@@ -3,6 +3,7 @@ package com.example.demo.service.impls;
 import com.example.demo.service.VerificationRequestService;
 import com.example.demo.repository.VerificationRequestRepository;
 import com.example.demo.entity.VerificationRequest;
+import com.example.demo.entity.CredentialRecord;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,9 @@ public class VerificationRequestImpls implements VerificationRequestService {
 
     @Override
     public VerificationRequest processVerification(Long requestId) {
-        VerificationRequest req = vrr.findById(requestId).orElseThrow(() -> new RuntimeException("Request not found"));
+        VerificationRequest req = vrr.findById(requestId).orElseThrow(() -> new ResourceNotFoundException("Request not found"));
+        CredentialRecord cred = credentialRepo.findById(req.getCredentialId()).orElseThrow(() -> new ResourceNotFoundException("Credential missing"));
+
         boolean valid = true;
         if (valid) {
             req.setStatus("SUCCESS");
