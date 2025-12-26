5145909ee6420512aa3a99fb5b15589bd54137b7
bala-amypo <bala@amypo.in>
2025-12-26 04:15:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/CredentialRecordRepository.java
+++ src/main/java/com/example/demo/repository/CredentialRecordRepository.java
@@ -7,12 +7,16 @@ import com.example.demo.entity.CredentialRecord;
 import java.util.List;
 import java.time.LocalDate;
 
+
 @Repository
 public interface CredentialRecordRepository extends JpaRepository<CredentialRecord, Long>{
     List<CredentialRecord> findByHolderId(Long holderId);
     CredentialRecord findByCredentialCode(String credentialCode);
 
     List<CredentialRecord> findByExpiryDateBefore(LocalDate date);
-    List<CredentialRecord> findByStatusUsingHql(String status);
+    
+    @Query("SELECT c FROM CredentialRecord c WHERE c.status = :status")
+    List<CredentialRecord> findByStatusUsingHql(@Param("status") String status);
+
     List<CredentialRecord> searchByIssuerAndType(String issuer, String credentialType);
 }
