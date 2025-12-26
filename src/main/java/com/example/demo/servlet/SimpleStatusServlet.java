944aa8a769775ef9275192f250da9cba33cbbdcb
bala-amypo <bala@amypo.in>
2025-12-26 03:15:17 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,5 +1,17 @@
 package com.example.demo.servlet;
 
-public class SimpleStatusServlet{
-    
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleStatusServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/plain");
+        response.getWriter().write("Digital Credential Verification Engine is running");
+    }
 }
