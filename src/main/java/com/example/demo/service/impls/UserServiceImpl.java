1acb9b20a67707e4727f3940a5dd8b2f12ee4a90
bala-amypo <bala@amypo.in>
2025-12-26 08:55:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impls/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impls/UserServiceImpl.java
@@ -1,29 +1,22 @@
 package com.example.demo.service.impls;
 
-import com.example.demo.service.UserService;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.entity.User;
-
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-import java.lang.Long;
-import java.lang.String;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository ur;
+    private final UserRepository repo;
 
-    @Override
-    public User registerUser(User user) {
-        return ur.save(user);
+    public UserServiceImpl(UserRepository repo) {
+        this.repo = repo;
     }
 
     @Override
-    public List<User> findByEmail(String email){
-    return ur.findByEmail(email);
+    public User findByEmail(String email) {
+        return repo.findByEmail(email);
     }
 }
